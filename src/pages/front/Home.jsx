ab617e4992be698b7652c28dafe8c7ce5e362133
Percy.Ku.Dev1 <percyku19@gmail.com>
2026-03-14 21:54:21 +0800

    initial create home page

--- src/pages/front/Home.jsx
+++ src/pages/front/Home.jsx
@@ -1,309 +1,45 @@
 const Home = () => {
   return (
-    <>
-      <div className="container">
-        <div className="row flex-md-row-reverse flex-column">
-          <div className="col-md-6">
-            <img
-              src="https://images.unsplash.com/photo-1526038335545-4b96864eaee7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1867&q=80
-          alt="
-              className="img-fluid"
-            />
-          </div>
-          <div className="col-md-6 d-flex flex-column justify-content-center mt-md-0 mt-3">
-            <h2 className="fw-bold">Lorem ipsum dolor sit</h2>
-            <h5 className="font-weight-normal text-muted mt-2">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor
-            </h5>
-            <div className="input-group mb-0 mt-4">
-              <input
-                type="text"
-                className="form-control rounded-0"
-                placeholder=""
-              />
-              <div className="input-group-append">
-                <button
-                  className="btn btn-dark rounded-0"
-                  type="button"
-                  id="search"
-                >
-                  Lorem ipsum
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row mt-5">
-          <div className="col-md-6 mt-md-4">
-            <div className="card border-0 mb-4 position-relative position-relative">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                className="card-img-top rounded-0"
-                alt="..."
-              />
-              <div className="card-body p-0">
-                <h4 className="mb-0 mt-4">Lorem ipsum</h4>
-                <div className="d-flex justify-content-between mt-3">
-                  <p className="card-text text-muted mb-0 w-75">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod.
-                  </p>
-                  <button className="btn btn-outline-dark rounded-0 text-nowrap">
-                    Lorem ipsum
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 mt-md-4">
-            <div className="card border-0 mb-4 position-relative position-relative">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                className="card-img-top rounded-0"
-                alt="..."
-              />
-              <div className="card-body p-0">
-                <h4 className="mb-0 mt-4">Lorem ipsum</h4>
-                <div className="d-flex justify-content-between mt-3">
-                  <p className="card-text text-muted mb-0 w-75">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod.
-                  </p>
-                  <button className="btn btn-outline-dark rounded-0 text-nowrap">
-                    Lorem ipsum
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 mt-md-4">
-            <div className="card border-0 mb-4 position-relative position-relative">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                className="card-img-top rounded-0"
-                alt="..."
-              />
-              <div className="card-body p-0">
-                <h4 className="mb-0 mt-4">Lorem ipsum</h4>
-                <div className="d-flex justify-content-between mt-3">
-                  <p className="card-text text-muted mb-0 w-75">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod.
-                  </p>
-                  <button className="btn btn-outline-dark rounded-0 text-nowrap">
-                    Lorem ipsum
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="col-md-6 mt-md-4">
-            <div className="card border-0 mb-4 position-relative position-relative">
-              <img
-                src="https://images.unsplash.com/photo-1502743780242-f10d2ce370f3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1916&q=80"
-                className="card-img-top rounded-0"
-                alt="..."
-              />
-              <div className="card-body p-0">
-                <h4 className="mb-0 mt-4">Lorem ipsum</h4>
-                <div className="d-flex justify-content-between mt-3">
-                  <p className="card-text text-muted mb-0 w-75">
-                    Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                    diam nonumy eirmod.
-                  </p>
-                  <button className="btn btn-outline-dark rounded-0 text-nowrap">
-                    Lorem ipsum
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="bg-light mt-7">
+    <main>
+      <section className="index-banner">
         <div className="container">
-          <div
-            id="carouselExampleControls"
-            className="carousel slide"
-            data-ride="carousel"
-          >
-            <div className="carousel-inner">
-              <div className="carousel-item active">
-                <div className="row justify-content-center py-7">
-                  <div className="col-md-8 d-flex">
-                    <img
-                      src="https://images.unsplash.com/photo-1490138139357-fc819d02e344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                      alt=""
-                      className="rounded-circle me-5"
-                      style={{
-                        width: "160px",
-                        height: "160px",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <div className="d-flex flex-column">
-                      <p className="h5">
-                        “Lorem ipsum dolor sit amet, consetetur sadipscing
-                        elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                        et dolore magna aliquyam erat.”
-                      </p>
-                      <p className="mt-auto text-muted">
-                        Lorem ipsum dolor sit amet.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row justify-content-center py-7">
-                  <div className="col-md-8 d-flex">
-                    <img
-                      src="https://images.unsplash.com/photo-1490138139357-fc819d02e344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                      alt=""
-                      className="rounded-circle me-5"
-                      style={{
-                        width: "160px",
-                        height: "160px",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <div className="d-flex flex-column">
-                      <p className="h5">
-                        “Lorem ipsum dolor sit amet, consetetur sadipscing
-                        elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                        et dolore magna aliquyam erat.”
-                      </p>
-                      <p className="mt-auto text-muted">
-                        Lorem ipsum dolor sit amet.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="carousel-item">
-                <div className="row justify-content-center py-7">
-                  <div className="col-md-8 d-flex">
-                    <img
-                      src="https://images.unsplash.com/photo-1490138139357-fc819d02e344?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1950&q=80"
-                      alt=""
-                      className="rounded-circle me-5"
-                      style={{
-                        width: "160px",
-                        height: "160px",
-                        objectFit: "cover",
-                      }}
-                    />
-                    <div className="d-flex flex-column">
-                      <p className="h5">
-                        “Lorem ipsum dolor sit amet, consetetur sadipscing
-                        elitr, sed diam nonumy eirmod tempor invidunt ut labore
-                        et dolore magna aliquyam erat.”
-                      </p>
-                      <p className="mt-auto text-muted">
-                        Lorem ipsum dolor sit amet.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <a
-              className="carousel-control-prev"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="prev"
-            >
-              <span
-                className="carousel-control-prev-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a
-              className="carousel-control-next"
-              href="#carouselExampleControls"
-              role="button"
-              data-slide="next"
-            >
-              <span
-                className="carousel-control-next-icon"
-                aria-hidden="true"
-              ></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-        </div>
-      </div>
-      <div className="container my-7">
-        <div className="row">
-          <div className="col-md-4">
-            <img
-              src="https://images.unsplash.com/photo-1548689816-c399f954f3dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-              alt=""
-              style={{
-                width: "48px",
-                height: "48px",
-                objectFit: "cover",
-              }}
-            />
-            <h4 className="mt-4">Lorem ipsum</h4>
-            <p className="text-muted">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna.
-            </p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://images.unsplash.com/photo-1548689816-c399f954f3dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-              alt=""
-              style={{
-                width: "48px",
-                height: "48px",
-                objectFit: "cover",
-              }}
-            />
-            <h4 className="mt-4">Lorem ipsum</h4>
-            <p className="text-muted">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna.
-            </p>
-          </div>
-          <div className="col-md-4">
-            <img
-              src="https://images.unsplash.com/photo-1548689816-c399f954f3dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-              alt=""
-              style={{
-                width: "48px",
-                height: "48px",
-                objectFit: "cover",
-              }}
-            />
-            <h4 className="mt-4">Lorem ipsum</h4>
-            <p className="text-muted">
-              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-              nonumy eirmod tempor invidunt ut labore et dolore magna.
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="bg-light py-7">
-        <div className="container">
-          <div className="row justify-content-center">
-            <div className="col-md-4 text-center">
-              <h3>Lorem ipsum</h3>
-              <p className="text-muted">
-                Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                diam nonumy eirmod.
+          <div className="row">
+            <div className="col-lg-10 d-flex align-items-start flex-column">
+              <h2 className="fs-5 fw-bold text-primary bg-white rounded-1 p-2 d-inline-block mb-4 ">
+                ZOBAA 旅行推薦平台
+              </h2>
+              <h1 className="fw-bold text-primary text-start">
+                <span className="d-block d-lg-inline">用旅行，</span>
+                <span> 收藏世界的樣子 </span>
+              </h1>
+
+              <p className="text-primary fs-6 text-start">
+                ZOBAA
+                整合熱門景點推薦、即時優惠、獨家行程，從比價到訂購，一次完成你的完美出遊計畫。
               </p>
-              <button className="btn btn-dark mt-4 rounded-0">
-                Lorem ipsum
-              </button>
+            </div>
+
+            <div className="col-lg-5">
+              <form className="position-relative">
+                <span className="material-icons-outlined align-bottom position-relative position-absolute top-50 start-0 translate-middle-y ps-4 text-neutral-60">
+                  search
+                </span>
+                <input
+                  type="search"
+                  className="form-control rounded-3 py-4 ps-10"
+                  id="navbar-lg-search"
+                  placeholder="搜尋景點、地點或城市"
+                />
+
+                <a className="btn btn-primary position-absolute flex-shrink-0 top-50 start-70 translate-middle-y">
+                  開始探索旅程
+                </a>
+              </form>
             </div>
           </div>
         </div>
-      </div>
-    </>
+      </section>
+    </main>
   );
 };
 
