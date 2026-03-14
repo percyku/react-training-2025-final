1ccca8349191ecd2d0af5e19fbb7061972ef0734
Percy.Ku.Dev1 <percyku19@gmail.com>
2026-03-14 21:48:43 +0800

    remove container

--- src/layout/FrontendLayout.jsx
+++ src/layout/FrontendLayout.jsx
@@ -1,17 +1,16 @@
 import { Outlet } from "react-router-dom";
-import Nav from "../components/Nav";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
 const FrontendLayout = () => {
   return (
-    <div className="container">
+    <>
       <Header />
       <main>
         <Outlet />
       </main>
       <Footer />
-    </div>
+    </>
   );
 };
 
