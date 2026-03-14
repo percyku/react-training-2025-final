ffe79039e0cedbc32dae6cf1d6205fa04a37e903
Percy.Ku.Dev1 <percyku19@gmail.com>
2026-03-14 21:53:56 +0800

    initial create header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,8 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { createAsyncGetCart } from "../slice/cartSlice";
 
+import logo from "../assets/images/logo.png";
+
 const Header = () => {
   const carts = useSelector((state) => state.cart.carts);
 
@@ -15,67 +17,47 @@ const Header = () => {
   const handleActvie = ({ isActive }) => {
     return isActive ? "nav-link ps-0 active-link" : "nav-link ps-0";
   };
-
-  const handleIconActvie = ({ isActive }) => {
-    return isActive ? "nav-link ps-0 active-link" : "nav-link ps-0";
-  };
   return (
-    <div className="bg-white sticky-top">
-      <div className="container">
-        <nav className="navbar px-0 navbar-expand-lg navbar-light bg-white">
-          <NavLink
-            className="navbar-brand position-absolute"
-            href="./index.html"
-            style={{
-              left: "50%",
-              transform: "translate(-50%, -50%)",
-              top: "50%",
-            }}
-          >
-            Navbar
-          </NavLink>
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNav"
-            aria-controls="navbarNav"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div
-            className="collapse navbar-collapse bg-white custom-header-md-open"
-            id="navbarNav"
-          >
-            <ul className="navbar-nav">
-              <li className="nav-item active">
-                <NavLink className={handleActvie} to="/product">
-                  產品列表
-                </NavLink>
-              </li>
-              <li className="nav-item">
-                <NavLink className={handleActvie} to="product/id">
-                  精選推薦
-                </NavLink>
-              </li>
-            </ul>
-          </div>
-          <div className="d-flex">
-            {/* <a href="#">
-              <i className="bi bi-heart-fill me-5"></i>
-            </a> */}
-            <NavLink to="cart" className=" position-relative">
-              <i className="bi bi-cart"></i>
-              <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-                {carts.length}
-              </span>
+    <>
+      <nav className="navbar navbar-animate py-4 py-lg-5 fixed-top">
+        <div className="container ">
+          <div className="d-flex align-items-center ">
+            <NavLink className="navbar-brand p-0 me-7" to="/">
+              <img src={logo} className="logo-size" alt="logo" />
             </NavLink>
           </div>
-        </nav>
-      </div>
-    </div>
+
+          <nav className="navbar px-0 navbar-expand me-auto">
+            <div
+              className="collapse navbar-collapse  custom-header-md-open"
+              id="navbarNav"
+            >
+              <ul className="navbar-nav">
+                <li className="nav-item active fs-5 fw-bold text-neutral-100 ">
+                  <NavLink className={handleActvie} to="/product">
+                    旅遊行程
+                  </NavLink>
+                </li>
+              </ul>
+            </div>
+          </nav>
+
+          <button type="button" className="btn py-2 border-0">
+            <NavLink to="cartcheckout">
+              <span className="position-relative">
+                <span className="position-absolute top-0 start-100 translate-middle badge rounded-circle bg-alert cart-tag p-0">
+                  {carts.length}
+                  <span className="visually-hidden">unread messages</span>
+                </span>
+                <span className="material-icons-outlined align-bottom">
+                  shopping_cart
+                </span>
+              </span>
+            </NavLink>
+          </button>
+        </div>
+      </nav>
+    </>
   );
 };
 
