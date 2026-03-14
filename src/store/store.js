ca2c5014276945ce936c5d6bf12444034eb2471a
Percy.Ku.Dev1 <percyku19@gmail.com>
2026-03-14 21:52:17 +0800

    remove adminProductreducer & loadingReducer ,add  orderReducer

--- src/store/store.js
+++ src/store/store.js
@@ -1,15 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import messageReducer from "../slice/messageSlice";
 import cartReducer from "../slice/cartSlice";
-import adminProductreducer from "../slice/adminProductSlice";
-import loadingReducer from "../slice/isLoadingSlice";
+import orderReducer from "../slice/orderSlice";
 
 export const store = configureStore({
   reducer: {
     message: messageReducer,
     cart: cartReducer,
-    adminProduct: adminProductreducer,
-    isLoading: loadingReducer,
+    order: orderReducer,
   },
 });
 
